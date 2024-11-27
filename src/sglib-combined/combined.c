bac51a4988831ccd732fffbb38a902e8b2d82126
jh2621 <jh2621@georgetown.edu>
2024-11-27 22:21:34 +0000

    Move sglib to unity build

--- src/sglib-combined/combined.c
+++ src/sglib-combined/combined.c
@@ -15,6 +15,12 @@
 #include "support.h"
 #include "sglib.h"
 
+/* All of the files to make this a unity build. */
+#include "main.c"
+#include "beebsc.c"
+#include "boardsupport.c"
+
+
 /* This scale factor will be changed to equalise the runtime of the
    benchmarks. */
 #define LOCAL_SCALE_FACTOR 29
